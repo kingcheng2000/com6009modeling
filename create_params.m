a1cec943ab76a0c5f05f76f32e63a5689f536331
IntoThePit <theblindorphan@gmail.com>
2018-05-07 20:53:36 +0100

    Moved some herring stuff to the global params (they are immutable)

--- create_params.m
+++ create_params.m
@@ -17,7 +17,7 @@ global PARAM
     % This part is to make it so that they always sum to 1:
     total_weights = hunt_weight + sep_weight + align_weight + cohes_weight;
     
-    PARAM.hunt_weight = hunt_weight/total_weights;
-    PARAM.sep_weight = sep_weight/total_weights;
-    PARAM.align_weight = align_weight/total_weights;
-    PARAM.cohes_weight = cohes_weight/total_weights;
+    PARAM.hunt_weight = hunt_weight/total_weights
+    PARAM.sep_weight = sep_weight/total_weights
+    PARAM.align_weight = align_weight/total_weights
+    PARAM.cohes_weight = cohes_weight/total_weights
